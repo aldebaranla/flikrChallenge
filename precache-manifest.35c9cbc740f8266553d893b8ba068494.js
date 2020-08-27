ad0b6b87b34963346d194320780e5608d8067bc9
aldebaranla <aldebaranla@hotmail.com>
2020-08-27 16:48:42 -0500

    Updates

---
+++ precache-manifest.35c9cbc740f8266553d893b8ba068494.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb06ea9c2de5d735812467f4559abbfe",
+    "url": "/flikrChallenge/index.html"
+  },
+  {
+    "revision": "becccf854ac43783bdca",
+    "url": "/flikrChallenge/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "f7997b6386c6a091f313",
+    "url": "/flikrChallenge/static/css/main.2634c19a.chunk.css"
+  },
+  {
+    "revision": "becccf854ac43783bdca",
+    "url": "/flikrChallenge/static/js/2.784589f7.chunk.js"
+  },
+  {
+    "revision": "a8eb76e4c6beb72c22bcc9f3c026930b",
+    "url": "/flikrChallenge/static/js/2.784589f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7997b6386c6a091f313",
+    "url": "/flikrChallenge/static/js/main.4940528c.chunk.js"
+  },
+  {
+    "revision": "f8798c09d3ba22e323a5",
+    "url": "/flikrChallenge/static/js/runtime-main.8b74975b.js"
+  }
+]);
